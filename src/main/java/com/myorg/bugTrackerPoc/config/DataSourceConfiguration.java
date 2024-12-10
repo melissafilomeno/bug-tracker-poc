fcf2788811bcfefc4855c418cf85353f20239d10
melissafilomeno <melissalfilomeno@gmail.com>
2024-12-09 20:51:33 -0500

    config server security and db credentials refresh

---
+++ src/main/java/com/myorg/bugTrackerPoc/config/DataSourceConfiguration.java
@@ -0,0 +1,40 @@
+package com.myorg.bugTrackerPoc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Profile("dev")
+@Configuration
+@RefreshScope
+public class DataSourceConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    @RefreshScope
+    public DataSource getDataSource(){
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
+                .build();
+    }
+
+}
